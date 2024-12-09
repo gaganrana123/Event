d32cabf6fe79cde0a06c6a867647a61a965742d0
Sanish Dangol <saniwalkerxd.999@gmail.com>
2024-12-09 20:06:24 +0545

    Second commit: Added MongoDB server connection and environment variables

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,24 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express();
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+// Load environment variables
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+const URI = process.env.MongoDuRI;
+
+console.log("MongoDB URI:", URI);
+
+// Connect to MongoDB
+mongoose
+  .connect(URI)
+  .then(() => console.log("Connected to mongoDB"))
+  .catch((error) => console.error("Error connecting to mongoDB:", error));
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
